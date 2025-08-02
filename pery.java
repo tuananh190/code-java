c04053fe60f16ff6f95857e0415abf112ff945a6
Lưu Tuấn Anh <139638161+tuananh190@users.noreply.github.com>
2025-08-02 23:23:04 +0700

    Thêm file mới có tên pery.java

---
+++ pery.java
@@ -0,0 +1,15 @@
+public class pery {
+    public static void main(String[] args) {
+
+        String name = "Pery";
+        int age = 25;   
+        String city = "New York";
+        while (age < 30) {
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("City: " + city);
+            age++; 
+        }
+
+}
+    }
